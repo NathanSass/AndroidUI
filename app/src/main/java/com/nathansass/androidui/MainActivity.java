0f1ab9b9f54dcd4fa74b3a3826fb29e14a8a8f10
Nathan Sass <nsass711@gmail.com>
2016-07-20 20:44:57 -0700

    have a basic looking screen

--- app/src/main/java/com/nathansass/androidui/MainActivity.java
+++ app/src/main/java/com/nathansass/androidui/MainActivity.java
@@ -1,7 +1,7 @@
 package com.nathansass.androidui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        int[] attrs = new int[]{R.attr.selectableItemBackground};
+//        TypedArray typedArray = getActivity().obtainStyledAttributes(attrs);
+//        int backgroundResource = typedArray.getResourceId(0, 0);
+//        myView.setBackgroundResource(backgroundResource);
     }
 }
